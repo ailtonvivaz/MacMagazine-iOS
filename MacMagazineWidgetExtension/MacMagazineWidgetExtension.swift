38577359b48b57123224d78fe14af34aafc1435e
Ailton Vieira Pinto Filho <ailtonvivaz@outlook.com>
2020-06-29 20:07:59 -0300

    committeste

--- MacMagazineWidgetExtension/MacMagazineWidgetExtension.swift
+++ MacMagazineWidgetExtension/MacMagazineWidgetExtension.swift
@@ -17,17 +17,12 @@ struct Provider: IntentTimelineProvider {
     }
 
     public func timeline(for configuration: ConfigurationIntent, with context: Context, completion: @escaping (Timeline<Entry>) -> ()) {
-        var entries: [SimpleEntry] = []
-
-        // Generate a timeline consisting of five entries an hour apart, starting from the current date.
         let currentDate = Date()
-        for hourOffset in 0 ..< 5 {
-            let entryDate = Calendar.current.date(byAdding: .hour, value: hourOffset, to: currentDate)!
-            let entry = SimpleEntry(date: entryDate, configuration: configuration)
-            entries.append(entry)
-        }
+        let refreshDate = Calendar.current.date(byAdding: .second, value: 10, to: currentDate)!
+        
+        let entry = SimpleEntry(date: currentDate, configuration: configuration)
 
-        let timeline = Timeline(entries: entries, policy: .atEnd)
+        let timeline = Timeline(entries: [entry], policy: .after(refreshDate))
         completion(timeline)
     }
 }
